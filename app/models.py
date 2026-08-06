21ca63ff05ce755f00f09dfca61062ba230eb506
Strykerxman <strykerxman@gmail.com>
2026-08-05 20:08:35 -0400

    refactor(models): migrate Idea class to SQLAlchemy ORM

--- app/models.py
+++ app/models.py
@@ -1,21 +1,30 @@
-from dataclasses import dataclass, field
 from datetime import datetime, timezone
 from enum import Enum
 
-class IdeaStatus(Enum):
+from app.database import Base
+from sqlalchemy import String, Text, Enum as SqlEnum, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+
+class IdeaStatus(str, Enum):
     ACTIVE = "active"
     PARKED = "parked"
     COMPLETED = "completed"
     DROPPED = "dropped"
 
 # todo: migrate to SQLAlchemy ORM
-@dataclass
-class Idea:
-    title: str
-    description: str
-    id: int | None = None
-    created_at: datetime = field(default_factory=lambda: datetime.now(timezone.utc))
-    status: IdeaStatus = IdeaStatus.PARKED
+class Idea(Base):
+    __tablename__ = "ideas"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(200), nullable=False) # not nullable to be searchable, identifiable and demonstrative (like on the webpage)
+    description: Mapped[str] = mapped_column(Text, default="", nullable=False)
+    
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default_factory=lambda: datetime.now(timezone.utc), 
+        nullable=False
+    )
+    status: Mapped[IdeaStatus] = mapped_column(default=IdeaStatus.PARKED, nullable=False)
         
 
 
